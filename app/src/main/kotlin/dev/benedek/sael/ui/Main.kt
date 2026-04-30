318fd402b7f642213c848dd8e05921ab05f224af
Benedek Szabó <bndkszabo@gmail.com>
2026-04-30 23:48:38 +0200

    Work on expanded state

--- app/src/main/kotlin/dev/benedek/sael/ui/Main.kt
+++ app/src/main/kotlin/dev/benedek/sael/ui/Main.kt
@@ -1,5 +1,6 @@
 package dev.benedek.sael.ui
 
+import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -7,6 +8,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -14,11 +16,14 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.LibraryMusic
 import androidx.compose.material.icons.outlined.Pause
 import androidx.compose.material.icons.outlined.PlayArrow
+import androidx.compose.material.icons.outlined.SkipNext
+import androidx.compose.material.icons.outlined.SkipPrevious
 import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.BottomSheetScaffoldState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
@@ -36,8 +41,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.GraphicsLayerScope
+import androidx.compose.ui.graphics.TransformOrigin
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalWindowInfo
 import androidx.compose.ui.res.painterResource
@@ -47,9 +56,12 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.lerp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.util.lerp
 import dev.benedek.sael.R
+import dev.benedek.sael.ui.reusable.animatePlayerElement
 import dev.benedek.sael.ui.theme.SaelTheme
 import kotlin.math.pow
+import kotlin.math.sqrt
 
 val miniPlayerContentHeight = 70.dp
 val miniPlayerHeight = miniPlayerContentHeight // TODO: set perfect height
@@ -59,7 +71,7 @@ val sheetCornerRadius = 12.dp // 24.dp is the default in materialtheme
 // I still don't understand why Compose doesn't have accessible properties like expanded ratio.
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun getCurrentSheetCornerRadius(scaffoldState: BottomSheetScaffoldState, navbarHeight: Dp): Dp {
+fun bottomSheetExpandedRatio(scaffoldState: BottomSheetScaffoldState, navbarHeight: Dp): Float {
     val density = LocalDensity.current
     val windowInfo = LocalWindowInfo.current
     val navbarHeightPx = with(density) { navbarHeight.toPx() }
@@ -78,6 +90,11 @@ fun getCurrentSheetCornerRadius(scaffoldState: BottomSheetScaffoldState, navbarH
 
         }
     }
+    return progress
+}
+
+@Composable
+fun getCurrentSheetCornerRadius(progress: Float): Dp {
     return lerp(0.dp, sheetCornerRadius, progress.pow(1/4f))
 }
 
@@ -92,7 +109,8 @@ fun Main() {
     )
     val density = LocalDensity.current
     var navBarHeightDp by remember { mutableStateOf(0.dp) }
-    val sheetCornerRadius = getCurrentSheetCornerRadius(scaffoldState, navBarHeightDp)
+    val progress = bottomSheetExpandedRatio(scaffoldState, navBarHeightDp)
+    val sheetCornerRadius = getCurrentSheetCornerRadius(progress)
     Box(
         modifier = Modifier.fillMaxSize()
     ) {
@@ -105,7 +123,7 @@ fun Main() {
                 )
             },
             sheetContent = {
-                Player()
+                Player(progress)
             },
             sheetShape = RoundedCornerShape(topStart = sheetCornerRadius, topEnd = sheetCornerRadius),
             sheetDragHandle = {
@@ -155,48 +173,93 @@ fun Content(paddingValues: PaddingValues) {
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun Player() {
-    MiniPlayer()
-}
-
+fun GraphicsLayerScope.mao() {}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MiniPlayer() {
+fun Player(progress: Float) {
     Box() {
         val width = 32.dp
         val height = 4.dp
+        val density = LocalDensity.current
+        val windowInfo = LocalWindowInfo.current
 
 
         Row(
             modifier = Modifier.fillMaxSize(),
         ) {
-            Image(
-                painterResource(R.drawable.ic_launcher_background),
-                contentDescription = "icon",
-                contentScale = ContentScale.Crop,
-                modifier = Modifier.size(miniPlayerContentHeight)
-            )
-            Column(
+            Box(
+                modifier = Modifier
+                    .size(miniPlayerContentHeight)
+                    .animatePlayerElement(
+                        progress,
+                        with(density) { miniPlayerContentHeight.toPx() },
+                        windowInfo.containerSize.width * (3/4f),
+                        windowInfo.containerSize.width / 2f,
+                        windowInfo.containerSize.height * 1 / 3f
+                    )
+            ) {
+                Image(
+                    painterResource(R.drawable.ic_launcher_background),
+                    contentDescription = "icon",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier.size(miniPlayerContentHeight)
+                )
+            }
+
+            Box(
                 modifier = Modifier
                     .weight(1f)
-                    .padding(10.dp),
-                verticalArrangement = Arrangement.Center
+                    .padding(10.dp)
+                    .animatePlayerElement(
+                        progress,
+                        null,
+                        null,
+                        windowInfo.containerSize.width * 3 / 8f,
+                        windowInfo.containerSize.height * 3 / 5f
+                    )
             ) {
-                Text("Title")
-                Text("Artist")
-            }
-            Row(
-                modifier = Modifier.padding(10.dp),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                IconButton({/*TODO()*/}) {
-                    Icon(Icons.Outlined.PlayArrow, null)
+                Column(
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Text("Title")
+                    Text("Artist")
                 }
-                IconButton({/*TODO()*/}) {
-                    Icon(Icons.Outlined.Pause, null)
+            }
+            Box(modifier = Modifier
+                .offset(40.dp)
+                .padding(10.dp)
+                .animatePlayerElement(
+                    progress,
+                    null,
+                    null,
+                    windowInfo.containerSize.width * 1 / 2f,
+                    windowInfo.containerSize.height * 4 / 5f
+                )) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    val context = LocalContext.current
+                    IconButton(
+                        onClick = {
+                            Toast.makeText(context, "Implement please", Toast.LENGTH_SHORT)
+                                .show()
+                        },
+                        modifier = Modifier.graphicsLayer { alpha = 1 - sqrt(progress) },
+                        enabled = progress < 0.999f
+                        ) {
+                        Icon(Icons.Outlined.SkipPrevious, null)
+                    }
+                    IconButton({/*TODO()*/}) {
+                        Icon(Icons.Outlined.PlayArrow, null)
+                    }
+                    IconButton(
+                        onClick = {/*TODO()*/},
+                        modifier = Modifier.graphicsLayer { alpha = 1 - sqrt(progress) },
+                        enabled = progress < 0.999f
+                        ) {
+                        Icon(Icons.Outlined.SkipNext, null)
+                    }
                 }
             }
         }
@@ -214,6 +277,13 @@ fun MiniPlayer() {
             }
         }
     }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MiniPlayer() {
+
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
