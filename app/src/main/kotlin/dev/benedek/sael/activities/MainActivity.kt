558b64bd49a62b48ca4ef7ad9bfe85ae21a1dbe0
Benedek Szabó <bndkszabo@gmail.com>
2026-05-10 17:00:04 +0200

    Create a media controller

--- app/src/main/kotlin/dev/benedek/sael/activities/MainActivity.kt
+++ app/src/main/kotlin/dev/benedek/sael/activities/MainActivity.kt
@@ -1,20 +1,35 @@
 package dev.benedek.sael.activities
 
+import android.content.ComponentName
+import android.media.browse.MediaBrowser
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.platform.LocalWindowInfo
+import androidx.core.content.ContextCompat
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.session.MediaController
+import androidx.media3.session.SessionCommand
+import androidx.media3.session.SessionResult
+import androidx.media3.session.SessionToken
+import com.google.common.util.concurrent.ListenableFuture
+import com.google.common.util.concurrent.MoreExecutors
+import dev.benedek.sael.services.PlaybackService
 import dev.benedek.sael.ui.LocalIsLandscape
 import dev.benedek.sael.ui.Main
 import dev.benedek.sael.ui.theme.SaelTheme
+import kotlin.jvm.java
 
 class MainActivity : ComponentActivity() {
+    private lateinit var controllerFuture: ListenableFuture<MediaController>
+    private var controller: MediaController? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
 
         setContent {
             val windowInfo = LocalWindowInfo.current
@@ -29,5 +44,54 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        // Create a SessionToken
+        val sessionToken = SessionToken(
+            this,
+            ComponentName(this, PlaybackService::class.java)
+        )
+
+        // Create a MediaController
+        controllerFuture = MediaController
+            .Builder(this, sessionToken)
+            // setListener currently does nothing. TODO: implement if needed
+            .setListener(
+                object : MediaController.Listener {
+                    override fun onCustomCommand(
+                        controller: MediaController,
+                        command: SessionCommand,
+                        args: Bundle
+                    ): ListenableFuture<SessionResult> {
+                        // Handle custom command. TODO
+                        return super.onCustomCommand(controller, command, args)
+                    }
+
+                    override fun onDisconnected(controller: MediaController) {
+                        // Handle disconnection TODO
+                    }
+                }
+            )
+            .buildAsync()
+
+        controllerFuture.addListener(
+            {
+                // MediaController is available here with controllerFuture.get()
+                controller = controllerFuture.get()
+            },
+            ContextCompat.getMainExecutor(this),
+        )
+
+
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        controller = null
+        MediaController.releaseFuture(controllerFuture)
     }
 }
