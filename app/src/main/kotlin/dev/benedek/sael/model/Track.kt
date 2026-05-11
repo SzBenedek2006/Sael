b9f2be1d361ed2852e682cce9c793191aba6f6da
Benedek Szabó <bndkszabo@gmail.com>
2026-05-11 09:49:38 +0200

    Extend and move Track to data/model/

--- app/src/main/kotlin/dev/benedek/sael/model/Track.kt
+++
@@ -1,9 +0,0 @@
-package dev.benedek.sael.model
-import android.graphics.Bitmap
-
-data class Track(
-    val title: String,
-    val artist: String,
-    val artwork: Bitmap?,
-    val duration: Long
-)
