d3ba4fe1fb8cb02a07d2a7606397d08baa7a2a96
Benedek Szabó <bndkszabo@gmail.com>
2026-05-14 15:11:19 +0200

    Add Material 3 constants because google doesn't want to.

---
+++ app/src/main/kotlin/dev/benedek/sael/ui/M3Constants.kt
@@ -0,0 +1,12 @@
+/**
+ * This will contain the internal or private constants that Google's material 3 compose library uses.
+ * This is to maintain consistency.
+ */
+
+package dev.benedek.sael.ui
+
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+val navigationBarHeight: Dp = 80.dp
+
